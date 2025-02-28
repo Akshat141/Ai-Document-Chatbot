9dd84a0bfecf1780929f5641fd71b323421f7ecc
Akshat <akshatsinghaadi@gmail.com>
2025-02-28 15:03:07 +0530

    Second changes Adding Google button

--- AuthenticationScript.js
+++ AuthenticationScript.js
@@ -8,6 +8,19 @@ LoginLink.addEventListener('click',function(){
     container.classList.remove('active')
 })
 
+document.getElementById("togglePassword").addEventListener("click", function () {
+  let passwordField = document.getElementById("password");
+  if (passwordField.type === "password") {
+    passwordField.type = "text";
+    this.classList.replace("ri-eye-off-fill", "ri-eye-fill");
+  } else {
+    passwordField.type = "password";
+    this.classList.replace("ri-eye-fill", "ri-eye-off-fill");
+  }
+});
+
+
+var t1 = gsap.timeline()
 
 gsap.from(".stars",{
     y:-100,
@@ -16,10 +29,17 @@ gsap.from(".stars",{
     duration:1,
 })
 
-gsap.from(".container",{
+t1.from(".container",{
     y: 150,
     opacity:0,
     delay:0.4,
     duration: 1.6,
     stagger:1
+})
+
+t1.from(".logo-container",{
+  y: -80,
+  opacity:0,
+  duration: 1,
+  stagger:1
 })

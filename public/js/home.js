f168dc475d2ec723ea14a1ff842b8e5eb05c970b
Akshat Singh <akshatsingh@Akshats-MacBook-Pro.local>
2025-03-07 00:03:52 +0530

    Chat-box,Logo changes

--- public/js/home.js
+++ public/js/home.js
@@ -1,6 +1,8 @@
 const typingText = document.getElementById('typing-text');
 const text = typingText.getAttribute('data-text');
 let index = 0;
+const sendbtn = document.querySelector(".send")
+const chatContainer = document.querySelector(".chat-container")
 
 function typeLetter() {
   if (index < text.length) {
@@ -12,3 +14,46 @@ function typeLetter() {
 
 typingText.innerHTML = '';
 requestAnimationFrame(typeLetter);
+
+sendbtn.addEventListener("click",function(event){
+  event.preventDefault();
+  
+})
+
+async function sendMessage() {
+  let userInput = document.querySelector("#user-input").value
+  let chatBox =  document.querySelector("#chat-box")
+if(userInput === ""){
+       alert("Write some thing")
+       typingText.style.display="none"
+   }else{
+       let userMessage = document.createElement("div")
+  userMessage.classList.add("message","user")
+  userMessage.innerHTML = `<p>${userInput}</p>`
+  chatBox.appendChild(userMessage)
+  document.querySelector("#user-input").value = ""
+  chatBox.scrollTop = chatBox.scrollHeight;
+
+  typingText.style.display="none"
+
+  let response = await fetch("http://localhost:5000/chat", {
+   method: "POST",
+   headers: { "Content-Type": "application/json" },
+   body: JSON.stringify({ message: userInput })
+});
+
+   let data = await response.json(); 
+   let botMessage = document.createElement("div")
+   botMessage.classList.add("message","bot")  
+   botMessage.innerHTML = `<p>${data.replay}</p>`
+   chatBox.appendChild(botMessage)
+   chatBox.scrollTop = chatBox.scrollHeight;
+   }
+}
+function uploadFile() {
+   let fileInput = document.getElementById("file-input");
+   let file = fileInput.files[0];
+   if (file) {
+       alert("File uploaded: " + file.name);
+   }
+}
